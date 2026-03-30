656ff952cfb36013a4d95160e126e7adf6766347
tapa2 <tarastertuchnu@gmail.com>
2026-03-30 21:15:35 +0300

    feat: fix SCD40 logic and successful build

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,15 +1,48 @@
 #include <Arduino.h>
+#include <SensirionI2cScd4x.h>
+#include <Wire.h>
+
+
+SensirionI2CScd4x scd4x; 
 
 void setup() {
-    // Налаштовуємо швидкість спілкування плати з комп'ютером
     Serial.begin(115200);
+    Wire.begin(21, 22); 
+
+    uint16_t error;
+    char errorMessage[256];
+
+    scd4x.begin(Wire);
+
+    error = scd4x.stopPeriodicMeasurement();
+    error = scd4x.startPeriodicMeasurement();
     
-    // Цей код виконається лише один раз при подачі живлення
-    Serial.println("Система мікроклімату: Запуск...");
+    if (error) {
+        Serial.print("Помилка запуску SCD4x: ");
+        // 2. ВИПРАВЛЕНО: errorToString (замість error_to_string)
+        errorToString(error, errorMessage, 256);
+        Serial.println(errorMessage);
+    }
 }
 
 void loop() {
-    // Цей код крутитиметься нескінченно
-    Serial.println("Опитування датчиків температури та вологості...");
-    delay(2000); // Робимо паузу 2 секунди, щоб не спамити в термінал
+    uint16_t co2;
+    float temperature;
+    float humidity;
+
+    // 3. Тепер scd4x буде розпізнано, бо ми правильно оголосили тип вище
+    uint16_t error = scd4x.readMeasurement(co2, temperature, humidity);
+
+    if (!error) {
+        Serial.print("CO2: "); Serial.print(co2);
+        Serial.print(" Temp: "); Serial.print(temperature);
+        Serial.print(" Hum: "); Serial.println(humidity);
+    } else {
+        Serial.print("Помилка читання: ");
+        char errorMsg[256];
+        errorToString(error, errorMsg, 256);
+        Serial.println(errorMsg);
+    }
+
+    delay(5000); 
 }
